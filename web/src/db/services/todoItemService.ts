5cf4ebaaf70b536d3e3179c4895f65ffe260b502
Vasiliya Karnatova <karnatova03@gmail.com>
2025-04-25 14:54:00 +0300

    adding queries for noteService and todoItemService

--- web/src/db/services/todoItemService.ts
+++ web/src/db/services/todoItemService.ts
@@ -1,1 +1,17 @@
 const TodoItem = require('../models/todoItem');
+
+import {ITodoItem} from '../interfaces/todoItem';
+
+export type ITodoItemUpdate = Partial<Pick<ITodoItem, 'todoItemTitle' | 'isChecked'>>;
+
+export const createTodo = async (noteId: string, todoItemTitle: string) => {
+    return TodoItem.query().insert({ noteId, todoItemTitle});
+};
+
+export const deleteTodo = async (todoItemId: string) => {
+    return TodoItem.query().deleteById(todoItemId);
+}
+
+export const updateTodo = async (TodoItemId: string, todoItemTitle: string, isChecked: boolean) => {
+    return TodoItem.query().patchAndFetch({ todoItemTitle, isChecked });
+};
