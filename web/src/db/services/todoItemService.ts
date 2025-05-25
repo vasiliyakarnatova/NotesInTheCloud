db89f536a3489c6e27b216068ffe837d4fd77328
MiroslavVelikov <79000592+MiroslavVelikov@users.noreply.github.com>
2025-05-25 14:48:25 +0300

    Server layer
    * initializing database
    
    * remove errors in services
    
    * restructuring
    
    * updating package.json with server layer dependencies
    
    * server layer logic and docker compose file
    
    * adding queries for noteService and todoItemService
    
    * adding knexConnection file and fixing some probelms about connection with database
    
    * fixing export knexInstance
    
    * adding queries for crud operations for user, note and todoItem
    
    * testing creating note
    
    * adding notification and reminder queries and fixing some bugs in note service and reminder model
    
    * adding comments about logic in notification and reminder services
    
    * todo routes are working and added middlewares to all routes
    
    * restructuring
    
    ---------
    
    Co-authored-by: Vasiliya Karnatova <karnatova03@gmail.com>

--- web/src/db/services/todoItemService.ts
+++ web/src/db/services/todoItemService.ts
@@ -6,14 +6,28 @@ import { v4 as uuidv4 } from 'uuid';
 
 export type ITodoItemUpdate = Partial<Pick<ITodoItem, 'todoItemTitle' | 'isChecked'>>;
 
+export const getTodos = async (noteId: string) => {
+    const todos = await TodoItem.query()
+            .select('todo_item.*')
+            .where('todo_item.note_id', noteId);
+    return todos;
+}
+
 export const createTodo = async (noteId: string, todoItemTitle: string) => {
     const todoItemId = uuidv4(); 
     const createdTime = new Date().toISOString();
-    return TodoItem.query().insert({ todoItemId, noteId, todoItemTitle, isChecked: false, createdTime});
+    return await TodoItem.query().insert({ todoItemId, noteId, todoItemTitle, isChecked: false, createdTime});
 };
 
 export const deleteTodo = async (todoItemId: string) => {
-    return TodoItem.query().deleteById(todoItemId);
+    const todoForDeleting = await TodoItem.query().findOne({ todo_item_id: todoItemId });
+    console.log(todoForDeleting);
+    const isDeleted = await TodoItem.query().deleteById(todoItemId);
+    if (isDeleted != 1) {
+        return undefined;
+    }
+
+    return todoForDeleting;
 }
 
 export const updateTodo = async (todoItemId: string, data: ITodoItemUpdate) => {
